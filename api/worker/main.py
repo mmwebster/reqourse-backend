cc266c6fdc2a0dbf0dd6cf8f35b0e6f408534dff
mmwebster <milo.webster@gmail.com>
2016-01-30 11:05:56 -0800

    WIP: worker

---
+++ api/worker/main.py
@@ -0,0 +1,27 @@
+# import data types
+# from ..utilities.dataTypes import Course
+
+class Course:
+    def __init__(self, subject="", number="", title="", units="", prereqs="", concurrent="", coreqs=""):
+        self.subject = subject
+        self.number = number
+        self.title = title
+        self.units = units
+        self.prereqs = prereqs
+        self.concurrent = concurrent
+        self.coreqs = coreqs
+        pass
+
+class CourseTuple:
+    def __init__(self, num="", courses = [], coursetuples = []):
+        self.num = num
+        self.courses = courses
+        self.coursetuples = coursetuples
+
+print("Hello");
+
+courselist = []
+tuplelist = []
+
+# def courseEval(course):
+#     
--- worker/main.py
+++ worker/main.py
@@ -1,8 +1,8 @@
 # import data types
-# from ..utilities.dataTypes import Course
+# from ..utilities import dataTypes
 
 class Course:
-    def __init__(self, subject="", number="", title="", units="", prereqs="", concurrent="", coreqs=""):
+    def __init__(self, subject = "", number = 0, title = "", units = 0, prereqs = [], concurrent = [], coreqs = []):
         self.subject = subject
         self.number = number
         self.title = title
@@ -13,15 +13,35 @@ class Course:
         pass
 
 class CourseTuple:
-    def __init__(self, num="", courses = [], coursetuples = []):
+    def __init__(self, num = 0, courses = [], coursetuples = []):
         self.num = num
         self.courses = courses
         self.coursetuples = coursetuples
 
+class PlanQuarter:
+    def __init__(self, courses = []);
+        self.courses = courses;
+
+class CoursePlan:
+    def __init__(self, quarters = []);
+        assert type(quarters) 
+        self.quarters = quarters;
+
+
+
 print("Hello");
 
-courselist = []
-tuplelist = []
+x = Course("AMS", 10, "Applied Bullshit", 5);
+y = PlanQuarter([x]);
+z = CoursePlan([y]);
 
-# def courseEval(course):
-#     
+# def printCoursePlan(
+
+# courselist = []
+# tuplelist = []
+
+# Evaluation of an individual course, for whether or not it passes the constraints
+def courseEval(course, currentPlan, maxUnits):
+    assert type(course) == Course
+    assert type(currentPlan) == CoursePlan
+    assert type(maxUnits) == int
