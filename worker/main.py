5005157d6c408d31fc72bb27fc10d60c9080ef1c
mmwebster <milo.webster@gmail.com>
2016-01-30 11:51:37 -0800

    WIP: worker

--- worker/main.py
+++ worker/main.py
@@ -2,7 +2,7 @@
 # from ..utilities import dataTypes
 
 class Course:
-    def __init__(self, subject = "", number = 0, title = "", units = 0, prereqs = [], concurrent = [], coreqs = []):
+    def __init__(self, subject = "", number = "", title = "", units = 0, prereqs = [], concurrent = [], coreqs = []):
         self.subject = subject
         self.number = number
         self.title = title
@@ -18,24 +18,29 @@ class CourseTuple:
         self.courses = courses
         self.coursetuples = coursetuples
 
-class PlanQuarter:
-    def __init__(self, courses = []);
-        self.courses = courses;
+class Quarter:
+    def __init__(self, courses = [], units = 0):
+        self.courses = courses
+        self.units = units
 
 class CoursePlan:
-    def __init__(self, quarters = []);
+    def __init__(self, quarters = []):
         assert type(quarters) 
-        self.quarters = quarters;
+        self.quarters = quarters
 
+def printCoursePlan(plan):
+    assert type(plan) == CoursePlan
+    print("correct type")
+    print("Plan:")
+    for quarter in plan.quarters:
+        print(" (" + str(quarter.units) + "un)")
+        # for course in quarter.courses
+        #     print("-" + course.subject + str(course.number))
+        print("\n")
 
-
-print("Hello");
-
-x = Course("AMS", 10, "Applied Bullshit", 5);
-y = PlanQuarter([x]);
-z = CoursePlan([y]);
-
-# def printCoursePlan(
+            
+    
+    
 
 # courselist = []
 # tuplelist = []
@@ -45,3 +50,20 @@ def courseEval(course, currentPlan, maxUnits):
     assert type(course) == Course
     assert type(currentPlan) == CoursePlan
     assert type(maxUnits) == int
+
+def main():
+    # init courses and create them
+    courses = [Course("AMS", "10", "Applied Bullshit", 5), Course("PHYS", "5A", "Intro to Cuntimatics", 5), Course("PHYS", "5L", "Intro to Cuntimatics, Lab", 1)]
+    # init quarter
+    quarter = Quarter()
+    # add all courses to quarter
+    for course in courses:
+        print("Course is " + course.title);
+    # for i in range(len(courses)):
+    #     quarter.courses[i] = courses[i];
+    # init overall plan
+    plan = CoursePlan([quarter])
+    # print plan
+    printCoursePlan(plan);
+
+main()
