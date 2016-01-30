a0d7c9467f95c2f1f8d809b6f5f7c6131d07ad78
mmwebster <milo.webster@gmail.com>
2016-01-30 13:58:36 -0800

    WIP: working on individual course eval

--- worker/main.py
+++ worker/main.py
@@ -6,7 +6,7 @@
 # Class definitions
 ########################
 class Course:
-    def __init__(self, subject = "", number = "", title = "", units = 0, prereqs = [], concurrent = [], coreqs = []):
+    def __init__(self, subject = "", number = "", title = "", units = 0, prereqs = [], concurrent = [], coreqs = [], offered = {"fall": False, "winter": False, "spring": False}):
         self.subject = subject
         self.number = number
         self.title = title
@@ -14,6 +14,7 @@ class Course:
         self.prereqs = prereqs
         self.concurrent = concurrent
         self.coreqs = coreqs
+        self.offered = offered # default to false
 
 class CourseTuple:
     def __init__(self, num = 0, courses = [], coursetuples = []):
@@ -28,7 +29,6 @@ class Quarter:
     def getUnits(self):
         n = 0
         for course in self.courses:
-            print("---> is " + course.title)
             n += course.units
         return n
             
@@ -36,6 +36,9 @@ class CoursePlan:
     def __init__(self, quarters = []):
         assert all(isinstance(quarter, Quarter) for quarter in quarters)
         self.quarters = quarters
+    # get # of filled quarters
+    def getNumQuarters(self):
+        return len(self.quarters)
 
 
 
@@ -48,38 +51,56 @@ class CoursePlan:
 def printCoursePlan(plan):
     assert isinstance(plan, CoursePlan)
     print("Plan:")
-    for quarter in plan.quarters:
-        print(" (" + str(quarter.getUnits()) + "un)")
-        for course in quarter.courses:
-            print("-" + course.subject + str(course.number))
-        print("\n")
+    for i in range(len(plan.quarters)):
+        print(" Qtr " + str(i + 1) + ", " + str(plan.quarters[i].getUnits()) + " unit(s)")
+        for course in plan.quarters[i].courses:
+            print(" -" + course.subject + str(course.number))
 
 # Evaluation of an individual course, for whether or not it passes the constraints
-def courseEval(course, currentPlan, maxUnits):
+def courseEval(newCourse, currentPlan, maxUnits):
+    # confirm correct type
     assert type(course) == Course
     assert type(currentPlan) == CoursePlan
     assert type(maxUnits) == int
+    # course already placed in plan?
+    # inspect every quarter in plan
+    for quarter in currentPlan.quarters:
+        # inspect every course in quarter
+        for course in quarter.courses:
+            idPrevCourse = course.subject + course.number
+            idNewCourse = newCourse.subject + newCourse.number
+            if idPrevCourse is idNewCourse:
+                return False
 
 
+
+    # At very end if none of the conditions fail
+    return True
+
+    
+    
 
 
 ########################
 # Main
 ########################
 def main():
-    # init courses and create them
-    courses = [Course("AMS", "10", "Applied Bullshit", 5), Course("PHYS", "5A", "Intro to Cuntimatics", 5), Course("PHYS", "5L", "Intro to Cuntimatics, Lab", 1)]
-    # init quarter
-    quarter = Quarter()
-    # add all courses to quarter
-    for course in courses:
-        print("Course is " + course.subject + course.number);
-        quarter.courses.append(course) # = courses[i];
+    # init/create data structures
+    courses = [
+            [Course("AMS", "10", "Applied Bullshit", 5), Course("PHYS", "5A", "Intro to Cuntimatics", 5), Course("PHYS", "5L", "Intro to Cuntimatics, Lab", 1)], 
+            [Course("AMS", "20", "Applied Bullshit", 5), Course("PHYS", "5B", "Intro to Cuntimatics", 5), Course("PHYS", "5M", "Intro to Cuntimatics, Lab", 2)]
+            ]
+    quarters = [Quarter(courses[0]), Quarter(courses[1])]
+    plan = CoursePlan(quarters)
 
-    # for i in range(len(courses)):
-    # init overall plan
-    plan = CoursePlan([quarter])
     # print plan
     printCoursePlan(plan);
+
+    newCourse = Course("AMS", "20")
+    if courseEval(newCourse, plan, 19):
+        print("Course eval succeeded.")
+    else:
+        print("Course eval failed.")
+
 
 main()
