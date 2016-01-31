dfd1420e9c93a5ca85e618186cebf4a3d3bf7aff
mmwebster <milo.webster@gmail.com>
2016-01-30 16:17:37 -0800

    WIP: worker

--- worker/main.py
+++ worker/main.py
@@ -1,12 +1,21 @@
 # import data types
 # from ..utilities import dataTypes
+# from enum import Enum
+
+########################
+# Enum definitions
+########################
+# class Season(Enum):
+#     fall = False
+#     winter = False
+#     spring = False
 
 
 ########################
 # Class definitions
 ########################
 class Course:
-    def __init__(self, subject = "", number = "", title = "", units = 0, prereqs = [], concurrent = [], coreqs = [], offered = {"fall": False, "winter": False, "spring": False}):
+    def __init__(self, subject = "", number = "", title = "", units = 0, prereqs = [], concurrent = [], coreqs = [], seasonsOffered = {"fall": False, "winter": False, "spring": False}):
         self.subject = subject
         self.number = number
         self.title = title
@@ -14,17 +23,18 @@ class Course:
         self.prereqs = prereqs
         self.concurrent = concurrent
         self.coreqs = coreqs
-        self.offered = offered # default to false
+        self.seasonsOffered = seasonsOffered # default to false
 
 class CourseTuple:
-    def __init__(self, num = 0, courses = [], coursetuples = []):
-        self.num = num
+    def __init__(self, numRequired = 0, courses = [], coursetuples = []):
+        self.numRequired = numRequired
         self.courses = courses
         self.coursetuples = coursetuples
 
 class Quarter:
     def __init__(self, courses = [], season = ""):
         self.courses = courses
+        self.season = season
     # get total num of units in quarter
     def getUnits(self):
         n = 0
@@ -74,7 +84,9 @@ def courseEval(newCourse, currentPlan, maxUnits, currentQuarter):
             if idPrevCourse == idNewCourse:
                 return False
 
-    for season in currentQuarter
+    currentSeason = currentPlan.quarters[currentQuarter].season
+    if not newCourse.seasonsOffered[currentSeason]:
+        return False
 
 
     # At very end if none of the conditions fail
@@ -93,22 +105,20 @@ def main():
             [Course("AMS", "10", "Applied Bullshit", 5), Course("PHYS", "5A", "Intro to Cuntimatics", 5), Course("PHYS", "5L", "Intro to Cuntimatics, Lab", 1)], 
             [Course("AMS", "20", "Applied Bullshit", 5), Course("PHYS", "5B", "Intro to Cuntimatics", 5), Course("PHYS", "5M", "Intro to Cuntimatics, Lab", 2)]
             ]
-    quarters = [Quarter(courses[0]), Quarter(courses[1])]
+    quarters = [Quarter(courses[0], "fall"), Quarter(courses[1], "winter")]
     plan = CoursePlan(quarters)
 
     # print plan
     printCoursePlan(plan);
 
-    newCourse = Course("PHYS", "5C", "Intro to Cuntimatics", 5, [], [], [], {"fall":False, "winter":False, "spring":True})
+    # define course for testing
+    newCourse = Course("PHYS", "5C", "Intro to Cuntimatics", 5, [], [], [], {"fall":False, "winter":True, "spring":True})
 
-
-    ####### courseEval unit test ########
-    #newCourse = Course("PHYS", "5C", "Intro to Cuntimatics", 5)
-    if courseEval(newCourse, plan, 19):
+    # courseEval unit test 
+    if courseEval(newCourse, plan, 19, 1):
         print("Course eval succeeded.")
     else:
         print("Course eval failed.")
-    ######################################
 
 
 
