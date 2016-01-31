fa4c5989ef1be4c2d26e4854d6a74c66a4e4d4ac
mmwebster <milo.webster@gmail.com>
2016-01-31 00:20:41 -0800

    Worker individual course eval complete

--- worker/main.py
+++ worker/main.py
@@ -21,14 +21,14 @@ class CourseTuple:
         self.courseTuples = courseTuples
 
 class Course:
-    def __init__(self, subject = "", number = "", title = "", units = 0, preReqs = CourseTuple(), coReqs = CourseTuple(), concurrent = CourseTuple(), seasonsOffered = {"fall": False, "winter": False, "spring": False}):
+    def __init__(self, subject = "", number = "", title = "", units = 0, preReqs = CourseTuple(), coReqs = CourseTuple(), concurrentReqs = [], seasonsOffered = {"fall": False, "winter": False, "spring": False}):
         self.subject = subject
         self.number = number
         self.title = title
         self.units = units
         self.preReqs = preReqs
         self.coReqs = coReqs
-        self.concurrent = concurrent
+        self.concurrentReqs = concurrentReqs
         self.seasonsOffered = seasonsOffered # default to false
 
 class Quarter:
@@ -44,10 +44,11 @@ class Quarter:
             
 class CoursePlan:
     # @param completed is an array of completed courses (or equivalent)
-    def __init__(self, quarters = [], completed = []):
+    def __init__(self, quarters = [], completed = [], maxUnits = 0):
         assert all(isinstance(quarter, Quarter) for quarter in quarters)
         self.quarters = quarters
         self.completed = completed
+        self.maxUnits = maxUnits
     # get # of filled quarters
     def getNumQuarters(self):
         return len(self.quarters)
@@ -109,7 +110,7 @@ def printCoursePlan(plan):
 
 
 # Evaluation of an individual course, for whether or not it passes the constraints
-def courseEval(newCourse, currentPlan, maxUnits, currentQuarter):
+def courseEval(newCourse, currentPlan, maxUnits, currentQuarter, concurrentCourse):
     # confirm correct type
     assert isinstance(newCourse, Course)
     assert isinstance(currentPlan, CoursePlan)
@@ -129,14 +130,30 @@ def courseEval(newCourse, currentPlan, maxUnits, currentQuarter):
 
     # 3. All pre-reqs satisfied? 
     # @param offset=1 b/c checking pre-reqs so must be 1 qtr back
-    # print("Checking if preReqs satisfied for " + newCourse.subject + newCourse.number)
-    # if not satisfied(currentPlan, newCourse.preReqs, 1):
-    #     return False
+    print("Checking if preReqs satisfied for " + newCourse.subject + newCourse.number)
+    if not satisfied(currentPlan, newCourse.preReqs, 1):
+        return False
 
     # 4. All co-reqs satisfied? 
     # @param offset=0 b/c checking co-reqs so course can be in current quarter. This works b/c coReqs in the A1 tree are listed as dependent on the courses that they satisfy so the breadth first search will pull them out first and then they will be in the plan (if they worked) prior to checking if the course that can use it as a co-req works.
     print("Checking if coReqs satisfied for " + newCourse.subject + newCourse.number)
     if not satisfied(currentPlan, newCourse.coReqs, 0):
+        return False
+
+    totalUnits = currentPlan.quarters[currentQuarter].getUnits()
+
+    # 5. Determine if there's a concurrent-req and sum units for newCourse and/(or not and) concurrent course
+    if len(newCourse.concurrentReqs) > 0:
+        # add to total units that of the two concurrent-req courses
+        units = newCourse.concurrentReqs[0].units + newCourse.units
+        totalUnits += units
+        concurrentCourse["id"] = newCourse.concurrentReqs[0].subject + newCourse.concurrentReqs[0].number
+    else:
+        # add just the units of the og course
+        totalUnits += newCourse.units
+
+    # 6. Check if exceeded max num of units
+    if totalUnits > currentPlan.maxUnits:
         return False
 
 
@@ -153,27 +170,35 @@ def courseEval(newCourse, currentPlan, maxUnits, currentQuarter):
 def main():
     # init/create data structures
     # prev taken coursework
-    completedCourses = [Course("MATH", "21"), Course("CMPE", "16")];
+    completedCourses = [Course("MATH", "21")];
     courses = [
             [Course("AMS", "10", "Applied Bullshit", 5), Course("PHYS", "5A", "Intro to Cuntimatics", 5), Course("PHYS", "5L", "Intro to Cuntimatics, Lab", 1)], 
             [Course("AMS", "20", "Applied Bullshit", 5), Course("PHYS", "5B", "Intro to Cuntimatics", 5), Course("PHYS", "5M", "Intro to Cuntimatics, Lab", 2)]
             ]
-    quarters = [Quarter(courses[0], "fall"), Quarter(courses[1], "winter"), Quarter([], "spring")]
-    plan = CoursePlan(quarters, completedCourses)
+    quarters = [Quarter(courses[0], "fall"), Quarter(courses[1], "winter"), Quarter([Course("CMPE", "16")], "spring")]
+    plan = CoursePlan(quarters, completedCourses, 19)
 
     # preReq tuple
-    p5c = CourseTuple(3, [Course("CMPE", "16"), courses[0][0]], [CourseTuple(3, [courses[0][1], courses[1][1], Course("CMPE", "17")], []), CourseTuple(1, [courses[0][2], courses[1][2]], [])])
+    p5c = CourseTuple(2, [], [CourseTuple(2, [courses[0][1], courses[1][1]], []), CourseTuple(1, [courses[0][2], courses[1][2]], [])])
+    c5c = CourseTuple(2, [Course("CMPE", "16"), courses[0][0]], [])
+    concurrent = [Course("PHYS", "5N")]
 
 
     # print plan
     printCoursePlan(plan);
 
     # define course for testing
-    newCourse = Course("PHYS", "5C", "Intro to Cuntimatics", 5, CourseTuple(), p5c, CourseTuple(), {"fall":False, "winter":True, "spring":True})
+    newCourse = Course("PHYS", "5C", "Intro to Cuntimatics", 5, p5c, c5c, concurrent, {"fall":False, "winter":True, "spring":True})
 
     # courseEval unit test 
-    if courseEval(newCourse, plan, 19, 2):
-        print("Course eval succeeded.")
+    concurrentCourse = {"id": ""}
+    if courseEval(newCourse, plan, 19, 2, concurrentCourse):
+        # check if concurrent course was added
+        if concurrentCourse["id"]:
+            # add the concurrent course by its unique id (look up in the dictionary)
+            print("Course eval succeeded. And added conccurent course " + concurrentCourse["id"] + ".")
+        else:
+            print("Course eval succeeded.")
     else:
         print("Course eval failed.")
 
